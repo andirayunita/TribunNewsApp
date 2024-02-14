81a58f2849f85e4018249a82a63268c031e98744
Andira Yunita <andhirayp@gmail.com>
2024-02-14 22:16:04 +0700

    Initial Commit

---
+++ TribunNews/TribunNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TribunNewsApp.swift
+//  TribunNews
+//
+//  Created by Andira Yunita on 14/02/24.
+//
+
+import SwiftUI
+
+@main
+struct TribunNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
